4bcced280c28ed244b7f182b704b6bbc38766a0a
RaymanCarney <Raymancarney@gmail.com>
2024-03-02 12:55:18 -0500

    Finished part 5

--- index.js
+++ index.js
@@ -17,9 +17,8 @@ function newItem(url, left, bottom){
         inventory.append (inventoryItem)
     })
 }
-
+let inventory = document.createElement('div')
 function newInventory(){
-    let inventory = document.createElement('div')
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
     inventory.style.left = '0px'
